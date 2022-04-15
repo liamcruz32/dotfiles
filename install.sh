8de4ebcd026e60ec790a44f886e211bc91749d4f
liamcruz32 <liamcruz@protonmail.ch>
2022-04-15 11:09:03 -0600

    Stuff

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ for x in $(ls applications)
 do
     for y in $(ls applications/$x)
     do
+        mkdir -p "$HOME/.config/$x"
         ln -s -f "$PWD/applications/$x/$y" "$HOME/.config/$x/"
     done
 done
