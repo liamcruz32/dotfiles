bb561b1d2d1f8e0dba3310d39144711717845fbe
Liam Cruz <liamcruz@proton.me>
2023-04-03 10:46:54 -0600

    arch install package update

--- archinstall.sh
+++ archinstall.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sudo pacman -Syu
-sudo pacman -S herbstluftwm picom dunst polybar acpi rofi tmux vim network-manager-applet nitrogen arandr lightdm lxappearance blueman pcmanfm libreoffice-fresh transmission-gtk gthumb gimp firefox papirus-icon-theme arc-gtk-theme
+sudo pacman -Syu herbstluftwm picom dunst polybar acpi rofi tmux vim network-manager-applet nitrogen arandr lightdm lxappearance blueman pcmanfm libreoffice-fresh transmission-gtk gthumb gimp firefox papirus-icon-theme arc-gtk-theme pipewire --needed
