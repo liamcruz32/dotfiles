415fd36c2870e7b8d861e4c42138509c23050155
Liam Cruz <liamcruz@proton.me>
2023-04-03 10:27:47 -0600

    arch package install script

---
+++ archinstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo pacman -Syu
+
+sudo pacman -S herbstluftwm picom dunst polybar acpi rofi tmux vim network-manager-applet nitrogen arandr lightdm lxappearance blueman pcmanfm libreoffice-fresh transmission-gtk gthumb gimp firefox
