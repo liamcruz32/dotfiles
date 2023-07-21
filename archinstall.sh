452427307bd9040a7b99344cf0c0585d00a083e1
liamcruz32 <liamcruz@proton.me>
2023-07-21 09:30:06 -0600

    update

--- archinstall.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo pacman -Syu xorg-xinput leafpad tumbler webp-pixbuf-loader ffmpegthumbnailer totem evince herbstluftwm picom dunst polybar acpi rofi tmux vim network-manager-applet nitrogen arandr lightdm lxappearance blueman pcmanfm libreoffice-fresh transmission-gtk gthumb gimp firefox papirus-icon-theme arc-gtk-theme pipewire lightdm-gtk-greeter ttf-font-awesome noto-fonts flameshot --needed
